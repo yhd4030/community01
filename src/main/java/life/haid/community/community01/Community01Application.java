ce71f1b7dd2772d0950fccd8dd4e8584617e601b
yhd4030 <yhd4030@163.com>
2020-02-08 22:06:49 +0800

    项目学习1

---
+++ src/main/java/life/haid/community/community01/Community01Application.java
@@ -0,0 +1,13 @@
+package life.haid.community.community01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Community01Application.class, args);
+    }
+
+}
---
+++ src/test/java/life/haid/community/community01/Community01ApplicationTests.java
@@ -0,0 +1,13 @@
+package life.haid.community.community01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Community01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
