0e923c9bde4e04f943e8af4ffbf15c7506cee48e
yhd4030 <yhd4030@163.com>
2020-03-05 01:10:47 +0800

    项目学习16

---
+++ src/main/java/life/haid/community/community01/controller/FileController.java
@@ -0,0 +1,18 @@
+package life.haid.community.community01.controller;
+
+import life.haid.community.community01.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/img/aaa.png");
+        return fileDTO;
+    }
+}
